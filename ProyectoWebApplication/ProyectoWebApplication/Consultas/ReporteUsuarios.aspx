dfa20faae7597543411a0277fb64d0bd78fdeee8
ihugojimenez <hugofernando809@hotmail.com>
2016-10-20 19:12:07 -0400

    haciendo pruebas para el funcionamiento del reportViewer

---
+++ ProyectoWebApplication/ProyectoWebApplication/Consultas/ReporteUsuarios.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="ReporteUsuarios.aspx.cs" Inherits="ProyectoWebApplication.Consultas.ReporteUsuarios" %>
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br/>
+    <br/>
+    <div class="row">
+                <div class="col-lg-12 text-center">
+<rsweb:ReportViewer ID="UsuariosReportViewer" runat="server" Width="501px">                    
+                </div>
+            </div>
+    
+    </rsweb:ReportViewer>
+    <br/>
+    <br />
+
+</asp:Content>
---
+++ ProyectoWebApplication/ProyectoWebApplication/Consultas/ReporteUsuarios.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoWebApplication.Consultas
+{
+    public partial class ReporteUsuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Configurar(ReportViewer reportViewergenerico, DataSet data)
+        {
+            reportViewergenerico.LocalReport.DataSources.Clear();
+            reportViewergenerico.ProcessingMode = ProcessingMode.Local;
+
+            reportViewergenerico.LocalReport.ReportPath = @"Reportes\UsersReport.rdlc";
+
+            ReportDataSource source = new ReportDataSource("Usuarios", data);
+            reportViewergenerico.LocalReport.DataSources.Add(source);
+
+            reportViewergenerico.LocalReport.Refresh();
+        }
+    }
+}
